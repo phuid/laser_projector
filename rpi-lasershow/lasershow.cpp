60c9aca6c35f37dcc27fb11a12c1a2026db08e53
Simon Hrouda <75166353+phuid@users.noreply.github.com>
2022-12-28 15:40:15 +0100

    change lasershow exec

---
+++ rpi-lasershow/lasershow.cpp
@@ -0,0 +1,97 @@
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdexcept>
+#include <time.h>
+#include <unistd.h>
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <wiringPi.h>
+#include "mcp4822.h"
+#include "Points.h"
+#include "IldaReader.h"
+
+using namespace std;
+
+void onInterrupt(int);
+
+int main(int argc, char **argv) {
+
+    // Validate arguments.
+    if (argc < 3) {
+		cout << "ERROR: Arguments missing." << endl;
+		cout << "Required: [pointDelay] [fileName]" << endl;
+	    return 1;
+	}
+
+    // Read arguments.
+    int pointDelay = atoi(argv[1]);
+    string fileName = argv[2];
+    double frameDuration = 0.033; // ~30fps (1/30=0.033..).
+
+    // Setup hardware communication stuff.
+    wiringPiSetup();
+
+    if(!mcp4822_initialize()) {
+                printf("Failed to initialize MCP4822.\n");
+                return -1;
+        }
+
+    // Setup ILDA reader.
+    Points points;
+    IldaReader ildaReader;
+    if (ildaReader.readFile(fileName)) {
+        printf("Provided file is a valid ILDA file.\n");
+        ildaReader.getNextFrame(&points);
+        printf("Points loaded in first frame: %d\n", points.size);
+    } else {
+        printf("Error opening ILDA file.\n");
+        return(1);
+    }
+
+    // Subscribe program to exit/interrupt signal.
+    signal(SIGINT, onInterrupt);
+
+    // Start the scanner loop with the current time.
+    std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+    while(true) {
+        // Exit if no points found.
+        if (points.size == 0) break;
+
+        // Move galvos to x,y position. (4096 is to invert horizontally)
+	mcp4822_set_voltage(MCP_4822_CHANNEL_A, 4096-points.store[points.index*3]);
+        mcp4822_set_voltage(MCP_4822_CHANNEL_B, points.store[(points.index*3)+1]);
+
+	// Turn on/off laser diode.
+        if (points.store[(points.index*3)+2] == 1) digitalWrite(0, HIGH); 
+        else digitalWrite(0, LOW); 
+
+        // Maybe wait a while there.
+        if (pointDelay > 0) usleep(pointDelay);
+
+        // In case there's no more points in the current frame check if it's time to load next frame.
+        if (!points.next()) {
+            std::chrono::duration<double> elapsedSeconds = std::chrono::system_clock::now() - start;
+            if(elapsedSeconds.count() > frameDuration) {
+                start = std::chrono::system_clock::now();
+                digitalWrite(0, LOW);
+                ildaReader.getNextFrame(&points);
+            }
+        }
+    }
+
+    // Cleanup and exit.
+    ildaReader.closeFile();
+    mcp4822_deinitialize();
+    return (0);
+}
+
+// Function that is called when program needs to be terminated. 
+void onInterrupt(int) {
+    printf("Turn off laser diode.\n");
+    digitalWrite(0, LOW);
+    mcp4822_deinitialize();
+    printf("Program was interrupted.\n");
+    exit(1); 
+}
---
+++ rpi-lasershow/test.c
@@ -0,0 +1,66 @@
+/*==========================================================================================================*\
+ * MIT License
+ *
+ * Copyright (c) 2022 Pawel Kusinski
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+\*==========================================================================================================*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include "mcp4822.h"
+#include <math.h>
+
+int main(void)
+{
+	printf("MCP4822 Linux Test\n");
+
+	if(!mcp4822_initialize()) {
+		printf("Failed to initialize MCP4822.\n");
+		return -1;
+	}
+
+	uint16_t voltage = 0;
+	const double sine_step = 2 * M_PI / 4095.0;
+	double sine_val = 0.0;
+	double sine_res;
+
+	for(;;) {
+		/* channel A: sawtooth */
+		mcp4822_set_voltage(MCP_4822_CHANNEL_A, voltage);
+		++voltage;
+		if (voltage >= 4095)
+			voltage = 0;
+
+		/* channel B: sine */
+		sine_res = sin(sine_val);
+		sine_res += 1.0;
+		mcp4822_set_voltage(MCP_4822_CHANNEL_B, (uint16_t)((4095.0  / 2.0) * sine_res));
+
+
+		sine_val += sine_step;
+		if(sine_val >= (2 * M_PI))
+			sine_val = 0.0;
+
+		usleep(1000);
+	}
+
+	mcp4822_deinitialize();
+	return 0;
+}
