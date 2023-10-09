766da5b41cad82b036870a5dd22dd351ffb3bc3d
phuid <simon.hrouda@gmail.com>
2023-10-09 21:33:46 +0200

    menu advancements

--- UItest/main.cpp
+++ UItest/main.cpp
@@ -13,60 +13,8 @@
 #define ENCODER_BUTTON_PIN 23
 #include "encoder.h"
 
-template <typename T>
-struct menu_val
-{
-	T num;
-	T min;
-	T max;
-};
-
-template <typename T>
-void change_val(int16_t val_min, int16_t val_max, T val)
-{
-	if (encoder_pos > 0)
-	{
-		if (*val + encoder_pos <= val_max)
-			*val += encoder_pos;
-		else
-			*val = val_max;
-	}
-	else if (encoder_pos < 0)
-	{
-		if (*val + encoder_pos >= val_min)
-			*val += encoder_pos;
-		else
-			*val = val_min;
-	}
-	encoder_pos = 0;
-}
-
-enum menu_option_style
-{
-	NESTED_MENU = 0,
-	VALUE,
-	SELECTION,
-	FUNCTION,
-	TEXT
-};
-
-struct menu_option
-{
-	char *name;
-	menu_option_style style;
-
-	std::vector<menu_option> nested_options; // does this work????????
-	uint8_t nest_selected = 0;
-	uint8_t nest_scroll = 0;
-	menu_val<int16_t> value;
-	std::vector<std::string> selection; // value changes with selection
-	void (*function)(void);
-};
-
-void print_test()
-{
-	std::cout << "print test" << std::endl;
-}
+#define SCREEN_HEIGHT 4
+#include "menu.h"
 
 int main()
 {
@@ -74,7 +22,7 @@ int main()
 
 	/* Create a LCD given SCL, SDA and I2C address, 4 lines */
 	/* PCF8574 has default address 0x27 */
-	lcd_t *lcd = lcd_create(9, 8, 0x27, 4);
+	lcd_t *lcd = lcd_create(9, 8, 0x27, SCREEN_HEIGHT);
 
 	if (lcd == NULL)
 	{
@@ -96,42 +44,65 @@ int main()
 	wiringPiISR(encoder_pins[1], INT_EDGE_BOTH, *handle_enc_interrupts);
 	wiringPiISR(encoder_button_pin, INT_EDGE_FALLING, *handle_btn_interrupts);
 
+	lcd_create_char(lcd, PARENT_CHAR_NUM, parent_char);
+
 	std::vector<menu_option> menu = {
 			{
-					.name = (char *)"1 - nest",
+					.name = (char *)"nest",
 					.style = NESTED_MENU,
-					.nested_options = {
+					.nested_menu_options = {
 							{
-									.name = (char *)"1.1 - text",
+									.name = (char *)"text",
 									.style = TEXT,
 							},
-							{.name = (char *)"1.2 - func",
-							 .style = FUNCTION,
-							 .function = *print_test},
+							{
+									.name = (char *)"func",
+									.style = FUNCTION,
+									.function = *print_test,
+							},
 					},
 			},
 			{
-					.name = (char *)"2 - text",
+					.name = (char *)"text",
 					.style = TEXT,
 			},
 			{
-					.name = (char *)"3 - val - brightness",
+					.name = (char *)"val - brightness",
 					.style = VALUE,
 					.value = {50, 0, 100},
 			},
 			{
-					.name = (char *)"4 - func",
+					.name = (char *)"func",
 					.style = FUNCTION,
 					.function = *print_test,
+			},
+			{
+					.name = (char *)"select",
+					.style = SELECTION,
+					.nested_menu_options = {
+							{
+									.name = (char *)"text",
+									.style = TEXT,
+							},
+							{
+									.name = (char *)"text",
+									.style = TEXT,
+							},
+							{
+									.name = (char *)"text",
+									.style = TEXT,
+							},
+					},
 			},
 	};
 	uint8_t menu_selected = 0;
 	uint8_t menu_scroll = 0;
+	bool menu_option_active = 0; // selected option clicked
 
 	while (true)
 	{
 		lcd_pos(lcd, 0, 0);
-		change_val<int16_t *>(menu[2].value.min, menu[2].value.max, &menu[2].value.num);
+		menu_interact(&menu, &menu_selected, &menu_scroll, &menu_option_active, ROOT_MENU);
 		lcd_backlight_dim(lcd, (float)menu[2].value.num / 100.f);
 		lcd_printf(lcd, (char *)"brightness: %d%% ", menu[2].value.num);
 	}
