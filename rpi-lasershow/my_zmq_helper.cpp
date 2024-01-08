57d9bed7a65b6ae9b9712cf874ef497434876e0b
phuid <simon.hrouda@gmail.com>
2024-01-08 23:05:41 +0100

    add files

---
+++ rpi-lasershow/my_zmq_helper.cpp
@@ -0,0 +1,9 @@
+#include "zmq.hpp"
+#include <string>
+
+void publish_message(zmq::socket_t &publisher, std::string message_string)
+{
+  zmq::message_t msg;
+  msg.rebuild(message_string);
+  publisher.send(msg, zmq::send_flags::none);
+}
