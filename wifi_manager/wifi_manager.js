5fc3f65f757b98675cc0275f05282ddeb83afd54
phuid <simon.hrouda@gmail.com>
2024-02-19 20:18:02 +0100

    WIFI MANAGER WORKS

--- wifi_manager/wifi_manager.js
+++ wifi_manager/wifi_manager.js
@@ -24,41 +24,63 @@ const iw_operation_mode = [
 ];
 
 function sendState() {
-  var ssid = execSync("iwgetid --raw");
-  console.log("wifi_ssid: " + ssid);
-  publisher.send("wifi_ssid: " + ssid);
+  try {
+    var ssid = execSync("iwgetid --raw");
+    console.log("wifi_ssid: " + ssid);
+    publisher.send("wifi_ssid: " + ssid);
+  } catch (err) {
+    console.log(err);
+    try {
+      var ssid = "" + execSync("iw dev | grep ssid");
+      console.log("ssid", ssid);
+      ssid = ssid.substring(ssid.indexOf("ssid") + 5);
+      console.log("wifi_ssid: " + ssid);
+      publisher.send("wifi_ssid: " + ssid);
+    } catch (errr) {
+      console.log(errr);
+    }
+  }
 
-  var mode_raw = execSync("iwgetid --mode --raw");
+  try {
+    var mode_raw = execSync("iwgetid --mode --raw");
+    publisher.send("mode_raw: " + mode_raw);
+    console.log("mode_raw: " + mode_raw);
+    publisher.send("mode: " + iw_operation_mode[Number(mode_raw)]);
+    console.log("mode: " + iw_operation_mode[Number(mode_raw)]);
+  } catch (err) {
+    console.log(err);
+  }
 
-  publisher.send("mode_raw: " + mode_raw);
-  console.log("mode_raw: " + mode_raw);
-  publisher.send("mode: " + iw_operation_mode[Number(mode_raw)]);
-  console.log("mode: " + iw_operation_mode[Number(mode_raw)]);
-  
-  var hostname = execSync("hostname -I");
-  publisher.send("hostname: " + hostname);
-  console.log("hostname: " + hostname);
+  try {
+    var hostname = execSync("hostname -I");
+    publisher.send("hostname: " + hostname);
+    console.log("hostname: " + hostname);
+  } catch (err) {
+    console.log(err);
+  }
 
   console.log("save: ", save);
   publisher.send("wifi_setting: " + save.wifi_setting.toString());
   // publisher.send("wifi_state: " + );
 }
 
-function isAPmode(ifyes, ifnot) {
+function isAPmode() {
   try {
-    execSync('iw dev | grep "type AP"', (error, stdout, stderr) => {
-      console.log(`error:\n${error.message}\n`);
-      if (stdout.length > 0) {
-        console.log("yup, it is indeed AP :)");
-        ifyes();
-      } else {
-        console.log("noap :(");
-        ifnot();
-      }
-    });
+    var out = (ssid = execSync('iw dev | grep "type AP"'));
+    if (out.length > 0) {
+      console.log("yup, it is indeed AP :)");
+      return true;
+    } else {
+      console.log("noap :(");
+      return false;
+    }
   } catch (error) {
-    console.log("error:", error.message);
-    ifnot();
+    console.log("errRRRRRRRRRR:", error.message);
+    if (error.message.substring(0, 14) == "Command failed") {
+      return false;
+    } else {
+      throw error;
+    }
   }
 }
 
@@ -105,110 +127,12 @@ function togglewifi(mode) {
         }
       );
 
-      isAPmode(
-        () => {
-          console.log("disabling AP");
-          execSync(
-            "sudo systemctl disable hostapd dnsmasq && sudo systemctl unmask dhcpcd && sudo systemctl enable dhcpcd",
-            /*after boot restart dhcpcd with default config file in /etc/dhcpcd.conf*/
-            (error, stdout, stderr) => {
-              if (error) {
-                console.log(`error:\n${error.message}\n`);
-              }
-              if (stderr) {
-                console.log(`stderr:\n${stderr}\n`);
-              }
-              console.log(stdout);
-            }
-          );
-          console.log("reboot");
-          execSync("sudo reboot");
-        },
-        () => {
-          console.log("AP already disabled");
-        }
-      );
-    }
-
-    if (mode == "hotspot") {
-      isAPmode(
-        () => {
-          {
-            console.log(res);
-            console.log("AP already started");
-            //start dnsmasq and dhcpcd with custom config
-            //hostapd has been started as a service by system
-            execSync(
-              "sudo systemctl disable dnsmasq dhcpcd",
-              (error, stdout, stderr) => {
-                if (error) {
-                  console.log(`error:\n${error.message}\n`);
-                }
-                if (stderr) {
-                  console.log(`stderr:\n${stderr}\n`);
-                }
-                console.log(stdout);
-              }
-            );
-            execSync(
-              "sudo dhcpcd -f " +
-                path.join(__dirname, "../config_templates/dhcpcd.conf"),
-              (error, stdout, stderr) => {
-                if (error) {
-                  console.log(`error:\n${error.message}\n`);
-                }
-                if (stderr) {
-                  console.log(`stderr:\n${stderr}\n`);
-                }
-                console.log(stdout);
-              }
-            );
-            execSync(
-              "sudo dnsmasq -C " +
-                path.join(__dirname, "../config_templates/dnsmasq.conf"),
-              (error, stdout, stderr) => {
-                if (error) {
-                  console.log(`error:\n${error.message}\n`);
-                }
-                if (stderr) {
-                  console.log(`stderr:\n${stderr}\n`);
-                }
-                console.log(stdout);
-              }
-            );
-
-            execSync(
-              "sudo systemctl enable dnsmasq dhcpcd",
-              (error, stdout, stderr) => {
-                if (error) {
-                  console.log(`error:\n${error.message}\n`);
-                }
-                if (stderr) {
-                  console.log(`stderr:\n${stderr}\n`);
-                }
-                console.log(stdout);
-              }
-            );
-          }
-        },
-        () => {
-          //enable AP
-          console.log("ap before boot");
-          // disable dnsmasq & dhcpcd to start them again after boot with custom config
-          execSync(
-            "sudo systemctl disable dnsmasq dhcpcd",
-            (error, stdout, stderr) => {
-              if (error) {
-                console.log(`error:\n${error.message}\n`);
-              }
-              if (stderr) {
-                console.log(`stderr:\n${stderr}\n`);
-              }
-              console.log(stdout);
-            }
-          );
-          execSync("sudo systemctl enable hostapd", (error, stdout, stderr) => {
-            //will run it myself after boot
+      if (isAPmode()) {
+        console.log("disabling AP");
+        execSync(
+          "sudo systemctl disable hostapd dnsmasq && sudo systemctl unmask dhcpcd && sudo systemctl enable dhcpcd",
+          /*after boot restart dhcpcd with default config file in /etc/dhcpcd.conf*/
+          (error, stdout, stderr) => {
             if (error) {
               console.log(`error:\n${error.message}\n`);
             }
@@ -216,11 +140,107 @@ function togglewifi(mode) {
               console.log(`stderr:\n${stderr}\n`);
             }
             console.log(stdout);
-          });
-          console.log("reboot");
-          execSync("sudo reboot");
+          }
+        );
+        console.log("reboot");
+        execSync("sudo reboot");
+      } else {
+        console.log("AP already disabled");
+      }
+    }
+
+    if (mode == "hotspot") {
+      if (isAPmode()) {
+        {
+          console.log("AP already started");
+          //start dnsmasq and dhcpcd with custom config
+          //hostapd has been started as a service by system
+          execSync(
+            "sudo systemctl stop dnsmasq dhcpcd",
+            (error, stdout, stderr) => {
+              if (error) {
+                console.log(`error:\n${error.message}\n`);
+              }
+              if (stderr) {
+                console.log(`stderr:\n${stderr}\n`);
+              }
+              console.log(stdout);
+            }
+          );
+          execSync(
+            "sudo dhcpcd -f " +
+              path.join(__dirname, "../config_templates/dhcpcd.conf"),
+            (error, stdout, stderr) => {
+              if (error) {
+                console.log(`error:\n${error.message}\n`);
+              }
+              if (stderr) {
+                console.log(`stderr:\n${stderr}\n`);
+              }
+              console.log(stdout);
+            }
+          );
+          try {
+            execSync("sudo pkill dnsmasq");
+          } catch (err) {
+            console.log(err);
+          }
+          execSync(
+            "sudo dnsmasq -C " +
+              path.join(__dirname, "../config_templates/dnsmasq.conf"),
+            (error, stdout, stderr) => {
+              if (error) {
+                console.log(`error:\n${error.message}\n`);
+              }
+              if (stderr) {
+                console.log(`stderr:\n${stderr}\n`);
+              }
+              console.log(stdout);
+            }
+          );
+
+          //            execSync(
+          //              "sudo systemctl enable dnsmasq dhcpcd",
+          //              (error, stdout, stderr) => {
+          //                if (error) {
+          //                  console.log(`error:\n${error.message}\n`);
+          //                }
+          //                if (stderr) {
+          //                  console.log(`stderr:\n${stderr}\n`);
+          //                }
+          //                console.log(stdout);
+          //              }
+          //            );
         }
-      );
+      } else {
+        //enable AP
+        console.log("ap before boot");
+        // disable dnsmasq & dhcpcd to start them again after boot with custom config
+        execSync(
+          "sudo systemctl disable dnsmasq dhcpcd",
+          (error, stdout, stderr) => {
+            if (error) {
+              console.log(`error:\n${error.message}\n`);
+            }
+            if (stderr) {
+              console.log(`stderr:\n${stderr}\n`);
+            }
+            console.log(stdout);
+          }
+        );
+        execSync("sudo systemctl enable hostapd", (error, stdout, stderr) => {
+          //will run it myself after boot
+          if (error) {
+            console.log(`error:\n${error.message}\n`);
+          }
+          if (stderr) {
+            console.log(`stderr:\n${stderr}\n`);
+          }
+          console.log(stdout);
+        });
+        console.log("reboot");
+        execSync("sudo reboot");
+      }
     }
   }
 }
